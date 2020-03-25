49bd2beeb145a4a5ea6007b4b47aeb735c58f7a9
BerengereAujard <38696293+BerengereAujard@users.noreply.github.com>
2020-03-25 17:07:37 +0100

    Ajout mentions manquantes dans mentions légales

--- pages/mentions-legales.js
+++ pages/mentions-legales.js
@@ -10,9 +10,13 @@ const MentionsLegales = () => {
         <h2>Editeur</h2>
         <p>
           SCIC La MedNum ICI MONTREUIL - 135 BOULEVARD CHANZY - 93100 MONTREUIL
+          Adresse contact : contact@lamednum.coop
+          N° RCS : 831 673 892 R.C.S Bobigny
+          Capital social : 62,900 euros
         </p>
         <h2>Directeur de la publication</h2>
         <p>Emmanuel Vandamme, Président de la SCIC La MedNum</p>
+        <h2> Hébergeur <h2>
         <h2>Illustrations</h2>
         <p>
           <a href="https://fr.freepik.com/photos-vecteurs-libre/affaires">
