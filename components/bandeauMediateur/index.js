0f6ef3f7a011731686998d93baf0f3d68043a228
khalid62 <75318838+khalid62@users.noreply.github.com>
2020-12-02 19:59:05 +0300

    Update index.js

--- components/bandeauMediateur/index.js
+++ components/bandeauMediateur/index.js
@@ -10,17 +10,31 @@ const BandeauMediateur = () => (
       <div className="container content-container">
         <div>
           <h3>J’accède aux services publics près de chez moi</h3>
-          <iframe id=""
-            title=""
-            width="100%"
-            height="500"
-            src="https://github.com/cget-carto/France-services.git">
-          </iframe>    
+          <a
+              href="https://www.cohesion-territoires.gouv.fr/france-services#e0"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <img
+                src="/images/logo/FS.png"
+                alt="France Services"
+              />
+            </a>   
         </div>
       </div>
       <div className="container content-container">
         <div>
-          <p>Vous aimez le contact humain, cherchez un métier qui a du sens dans un secteur d’avenir ? Rejoignez le mouvement national des conseillers numériques</p>
+          <p>Vous aimez le contact humain, cherchez un métier qui a du sens dans un secteur d’avenir ?<br>Rejoignez le mouvement national des conseillers numériques</p>
+          <a
+              href="https://www.conseiller-numerique.gouv.fr/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <img
+                src="/images/logo/CN.png"
+                alt="Conseiller Numérique"
+              />
+            </a>  
         </div>
       </div>
     </div>
