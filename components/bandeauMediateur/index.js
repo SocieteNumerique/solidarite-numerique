7c0fdd570b9ed5e2715e7b2e72f0781a86bc05e2
khalid62 <75318838+khalid62@users.noreply.github.com>
2020-12-03 06:24:51 +0300

    Update index.js

--- components/bandeauMediateur/index.js
+++ components/bandeauMediateur/index.js
@@ -10,33 +10,10 @@ const BandeauMediateur = () => (
       <div className="container content-container">
         <div>
           <h3>J’accède aux services publics près de chez moi</h3>
-          <a
-              href="https://www.cohesion-territoires.gouv.fr/france-services#e0"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <img
-                src="/images/logo/FS.png"
-                alt="France Services"
-              />
-            </a>   
+           
         </div>
       </div>
-      <div className="container content-container">
-        <div>
-          <p>Vous aimez le contact humain, cherchez un métier qui a du sens dans un secteur d’avenir ?<br>Rejoignez le mouvement national des conseillers numériques</p>
-          <a
-              href="https://www.conseiller-numerique.gouv.fr/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <img
-                src="/images/logo/CN.png"
-                alt="Conseiller Numérique "
-              />
-            </a>  
-        </div>
-      </div>
+      
     </div>
 
     <style jsx>{`
