d47fd10ba90392236de623c5bf3f752f8ebd5ba8
khalid62 <75318838+khalid62@users.noreply.github.com>
2020-12-07 20:46:49 +0100

    Update index.js

--- components/bandeauMediateur/index.js
+++ components/bandeauMediateur/index.js
@@ -24,12 +24,12 @@ const BandeauMediateur = () => (
       </div> 
       <div className="container content-container">
         <div>
-          <p>Vous aimez le contact humain, cherchez un métier qui a du sens dans un secteur d’avenir ? Rejoignez le mouvement national des conseillers numériques!</p>
           <a
               href="https://www.conseiller-numerique.gouv.fr/"
               rel="noopener noreferrer"
               target="_blank"
             >
+              <p>Vous aimez le contact humain, cherchez un métier qui a du sens dans un secteur d’avenir ? Rejoignez le mouvement national des conseillers numériques!</p>
               <img
                 src="/images/logo/CN.png"
                 alt="Conseiller Numérique "
