f6462a93b3ff60869787d2398cba870250312f51
khalid62 <75318838+khalid62@users.noreply.github.com>
2020-12-07 21:13:06 +0100

    Update index.js

--- components/bandeauMediateur/index.js
+++ components/bandeauMediateur/index.js
@@ -23,7 +23,7 @@ const BandeauMediateur = () => (
         </div>
       </div> 
       <div className="container content-container">
-        <div className="row">
+        <div>
           <p>Vous aimez le contact humain, cherchez un métier qui a du sens dans un secteur d’avenir? Rejoignez le mouvement national des conseillers numériques!</p>
           <a
               href="https://www.conseiller-numerique.gouv.fr/"
@@ -58,10 +58,6 @@ const BandeauMediateur = () => (
       .container {
         display: flex;
         align-items: center;
-      }
-  
-      .row {
-        width: auto;
       }
   
       .container > div:first-of-type {
