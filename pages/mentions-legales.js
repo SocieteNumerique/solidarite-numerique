6d2dfad648d391aaa18993b49e9db55a898dd81f
khalid62 <75318838+khalid62@users.noreply.github.com>
2020-12-08 16:14:19 +0100

    Update mentions-legales.js

--- pages/mentions-legales.js
+++ pages/mentions-legales.js
@@ -33,49 +33,8 @@ const MentionsLegales = () => {
           Lepreux.
         </p>
         <p>
-        Les informations que vous nous confiez sont enregistrées dans un fichier informatisé par
-        L&apos;Agence Nationale de Cohésion des Territoires, 20, avenue de Ségur TSA 10717,
-        75 334 Paris Cedex 07<br />
-        La base légale du traitement de vos données est l&aposarticle 5.6 de la Loi n° 78-17 du 6 janvier
-        1978 relative à l&aposinformatique, aux fichiers et aux libertés.<br />
-        Nous recueillons vos données dans la poursuite de notre intérêt légitime: l&aposamélioration
-        du service que nous vous proposons pour répondre à vos besoins en accompagnement
-        numérique.<br />
-        Ainsi, nous recueillons votre numéro de téléphone portable, afin de retrouver votre
-        requête précédente dans le cas où vous nous rappeleriez, et de répondre au mieux à vos
-        besoins. Ce recueil nous permet également de vous envoyer des ressources par SMS, si
-        vous acceptez d&aposen recevoir.<br />
-        Nous collectons également, si vous acceptez de nous les confier:<br />
-        <b>
-        Votre code postal, qui nous permettra d&aposidentifier, par le biais d&aposune
-        enquête statistique anonymisée, les besoins spécifiques en
-        accompagnement numérique de chaque territoire;<br />
-        Vos nom et prénom, afin de personnaliser le service que nous vous
-        apportons;<br />
-        Votre adresse mail, si vous souhaitez que nous vous envoyons des
-        ressources.<br />
-        <b>
-        Nous conservons vos données pour une durée de trois ans suite à votre dernier appel.
-        Vos données sont accessibles par:<br />
-
-        L&apos;Agence Nationale de Cohésion des territoires, responsable du traitement<br />
-        Ses prestataires techniques, Aircall et la Régie de Quartiers de Trélazé, qui
-        stockent et sécurisent vos données;<br />
-        La MedNum, coordinateur du projet<br />
-        Hubik et Hinaura, partenaires formation<br />
-        Les médiateurs qui vous accompagnent.<br />
-
-        Vous pouvez nous demander d&aposaccéder à vos données personnelles, en demander la
-        rectification ou l&apos;effacement, ou encore solliciter une limitation du traitement vous
-        concernant.<br />
-        Vous pouvez également vous opposer au traitement et solliciter la portabilité de vos
-        données.<br />
-        Pour exercer ces droits ou pour toute question sur le traitement de vos données dans ce
-        dispositif, vous pouvez nous contacter à l&aposadresse suivante: dpo@anct.gouv.fr<br />
-        Si vous estimez, après nous avoir contactés, que vos droits « Informatique et Libertés &raquo; ne
-        sont pas respectés, vous pouvez adresser une réclamation à la CNIL.<br />
-        Vous pouvez également consulter le site <a href="http://cnil.fr">cnil.fr</a> pour plus d’informations sur vos droits.
-      </p>
+          Les informations que vous nous confiez sont enregistrées dans un fichier informatisé par L&apos;Agence Nationale de Cohésion des Territoires, 20, avenue de Ségur TSA 10717, 75 334 Paris Cedex 07
+        </p>
       </div>
     </Page>
   );
