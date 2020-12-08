4ef8c85be102e1f4ab69ba4b585904b68cc2a4d5
khalid62 <75318838+khalid62@users.noreply.github.com>
2020-12-08 18:04:07 +0100

    Update index.js

--- components/categories/index.js
+++ components/categories/index.js
@@ -18,7 +18,7 @@ const Categories = () => (
                 href={category.path}
                 className="resource dont-apply-link-style"
                 title={category.title}
-                {category.path='https://www.clic-connect.fr' ? target="_blank" : target="_self"}
+                target={category.path='https://www.clic-connect.fr' ? "_blank" : "_self"}
               >
                 <div>
                   <h3>
