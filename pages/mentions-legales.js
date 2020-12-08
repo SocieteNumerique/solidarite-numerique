e625fb8b5375585dfc7883b2fce7f7bf5364a322
khalid62 <75318838+khalid62@users.noreply.github.com>
2020-12-08 16:38:41 +0100

    Update mentions-legales.js

--- pages/mentions-legales.js
+++ pages/mentions-legales.js
@@ -33,7 +33,16 @@ const MentionsLegales = () => {
           Lepreux.
         </p>
         <p>
-          Les informations que vous nous confiez sont enregistrées dans un fichier informatisé par L&apos;Agence Nationale de Cohésion des Territoires, 20, avenue de Ségur TSA 10717, 75 334 Paris Cedex 07
+          Les informations que vous nous confiez sont enregistrées dans un fichier informatisé par L&apos;Agence Nationale de Cohésion des Territoires, 20, avenue de Ségur TSA 10717, 75 334 Paris Cedex 07<br />
+          La base légale du traitement de vos données est l’article 5.6 de la Loi n° 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés.<br />
+          Nous recueillons vos données dans la poursuite de notre intérêt légitime: l’amélioration du service que nous vous proposons pour répondre à vos besoins en accompagnement numérique.<br />
+          Ainsi, nous recueillons votre numéro de téléphone portable, afin de retrouver votre requête précédente dans le cas où vous nous rappeleriez, et de répondre au mieux à vos besoins. Ce recueil nous permet également de vous envoyer des ressources par SMS, si vous acceptez d’en recevoir.<br />
+          Nous collectons également, si vous acceptez de nous les confier:<br />
+          <b>
+            Votre code postal, qui nous permettra d’identifier, par le biais d’une enquête statistique anonymisée, les besoins spécifiques en accompagnement numérique de chaque territoire;<br />
+            Vos nom et prénom, afin de personnaliser le service que nous vous apportons;<br />
+            Votre adresse mail, si vous souhaitez que nous vous envoyons des ressources.<br />
+          </b>
         </p>
       </div>
     </Page>
