51b5c3c3e3c2f2c17e302bead95d93258842b4ef
khalid62 <75318838+khalid62@users.noreply.github.com>
2021-01-09 21:40:56 +0100

    Update index.js

--- components/footer/index.js
+++ components/footer/index.js
@@ -5,243 +5,57 @@ import constants from '../../constants';
 const Footer = () => {
   return (
     <>
-      <footer id="footer">
-        <div className="footer__social">
-          <div>N'hésitez pas à partager ce site sur les réseaux sociaux</div>
-          <div className="separator">➞</div>
-          <div>
-            <div>
-              <a
-                href="https://twitter.com/intent/tweet?text=https://solidarite-numerique.fr/"
-                title="Twitter"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src="/images/social/twitter.svg"
-                  alt="Twitter"
-                  className="icon icon-twitter"
-                />
-              </a>
-            </div>
-            <div>
-              <a
-                href="https://www.facebook.com/sharer/sharer.php?u=https://solidarite-numerique.fr"
-                title="Facebook"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src="/images/social/facebook.svg"
-                  alt="Facebook"
-                  className="icon icon-facebook"
-                />
-              </a>
-            </div>
-            <div>
-              <a
-                href="mailto:exemple@adresse-mail.com?subject=Solidarite-numerique&body=https://solidarite-numerique.fr"
-                title="Partager par email"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  src="/images/social/email.svg"
-                  alt="Email"
-                  className="icon icon-mail"
-                />
-              </a>
-            </div>
+      <footer class="rf-footer" role="contentinfo" id="footer">
+          <div class="rf-container">
+              <div class="rf-footer__body">
+                  <div class="rf-footer__brand">
+                      <a class="rf-logo" href="#" title="république française">
+                          <span class="rf-logo__title">république
+                              <br>française</span>
+                      </a>
+                  </div>
+                  <div class="rf-footer__content">
+                      <p class="rf-footer__content-desc"></p>
+                      <ul class="rf-footer__content-list">
+                          <li>
+                              <a class="rf-footer__content-link" href="http://legifrance.gouv.fr">legifrance.gouv.fr</a>
+                          </li>
+                          <li >
+                              <a class="rf-footer__content-link" href="http://gouvernement.fr">gouvernement.fr</a>
+                          </li>
+                          <li >
+                              <a class="rf-footer__content-link" href="http://service-public.fr">service-public.fr</a>
+                          </li>
+                          <li >
+                              <a class="rf-footer__content-link" href="http://data.gouv.fr">data.gouv.fr</a>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+              <div class="rf-footer__bottom">
+                  <ul class="rf-footer__bottom-list">
+                      <li class="rf-footer__bottom-item">
+                          <a class="rf-footer__bottom-link" href="#">Plan du site</a>
+                      </li>
+                      <li class="rf-footer__bottom-item">
+                          <a class="rf-footer__bottom-link" href="#">Accessibilité: non/partiellement/totalement conforme</a>
+                      </li>
+                      <li class="rf-footer__bottom-item">
+                          <a class="rf-footer__bottom-link" href="#">Mentions légales</a>
+                      </li>
+                      <li class="rf-footer__bottom-item">
+                          <a class="rf-footer__bottom-link" href="#">Données personnelles</a>
+                      </li>
+                      <li class="rf-footer__bottom-item">
+                          <a class="rf-footer__bottom-link" href="#">Gestion des cookies</a>
+                      </li>
+                  </ul>
+                  <div class="rf-footer__bottom-copy">
+                      © République Française 2020
+                  </div>
+              </div>
           </div>
-        </div>
-        <div className="container content-container">
-          <div className="partenaire layout-center">
-            <a
-              href="https://www.lamednum.coop/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <img
-                src="/images/logo/logo_mednum.svg"
-                alt="Une initiative de la MedNum"
-              />
-            </a>
-            <a
-              href="https://agence-cohesion-territoires.gouv.fr"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <img
-                src="/images/logo/logo_anct.png"
-                alt="ANCT"
-              />
-            </a>
-            <a
-              href="https://www.gouvernement.fr/france-relance"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <img
-                src="/images/logo/FR.png"
-                alt="France Relance"
-              />
-            </a>
-            <a
-              href="https://www.gouvernement.fr/le-secretariat-d-etat-charge-du-numerique/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              avec le soutien du
-              <img
-                className="logo"
-                src="/images/logo/logo_secnum.png"
-                alt="En partenariat avec le gouvernement"
-              />
-            </a>
-          </div>
-          <ul className="footer__links">
-            <li>
-              <a href="/contact">Nous contacter</a>
-            </li>
-            <li>
-              <a href="/mentions-legales">Mentions Légales</a>
-            </li>
-            <li>
-              <a href="/a-propos">Qui sommes-nous ?</a>
-            </li>
-            <li>
-              <a href="/vie-privee">Politique de confidentialité</a>
-            </li>
-          </ul>
-        </div>
       </footer>
-      <style jsx>{`
-        #footer {
-          background: #EBEFF3;
-          color: #26353F;
-          font-size: 0.9rem;
-          padding: 0 0 2rem 0;
-        }
-
-        a {
-          opacity: 0.7;
-          text-decoration: none;
-          color: #272c32;
-          display: block;
-        }
-        a:hover,
-        a:focus {
-          opacity: 1;
-        }
-
-        ul {
-          list-style-type: none;
-          padding: 0;
-        }
-
-        .footer__links {
-          display: flex;
-          width: 100%;
-          flex-direction:column;
-          justify-content: center;
-          margin: 0;
-          line-height: 2em;
-          flex-grow:0;
-          align-items: flex-end;
-        }
-
-        .footer__links li > a{
-          padding: 6px 20px;
-          display: block;
-        }
-
-        .footer__social {
-          height: auto;
-          margin: 0 auto 30px;
-          color: #fff;
-          background-color: ${constants.colors.darkBlue};
-          padding:20px 0;
-          display: flex;
-          flex-direction: row;
-          justify-content: center;
-          align-items: center;
-          font-size:1rem;
-        }
-
-        .footer__social .separator {
-          margin: 0 20px;
-        }
-
-        .footer__social >div:last-of-type {
-          display:flex;
-          flex-direction:row;
-          margin: 5px 0;
-        }
-
-        .footer__social img {
-          width: 20px;
-          padding-left: 10px;
-        }
-        .footer__social a:hover {
-          background: none;
-        }
-
-        .partenaire a {
-          opacity: 1;
-          margin:0 20px;
-        }
-        .partenaire {
-          margin: auto;
-          margin 20px 0;
-        }
-
-        .partenaire img {
-          width: auto;
-          max-width: 209px;
-          height: 82px;
-          margin:3px;
-        }
-
-        .container {
-          position: relative;
-          max-width: 1000px;
-          margin: 0 auto;
-          padding: 0 20px;
-          display: flex;
-          flex-flow: row;
-          justify-content: space-between;
-        }
-        .container h1:first-of-type {
-          margin-top: 0;
-        }
-
-        @media only screen and (min-width: 1px) and (max-width: 800px) {
-          .footer__social {
-            flex-direction: column;
-            padding:20px 25px;
-            text-align:center;
-          }
-          .footer__social > div:first-of-type {
-            margin-bottom:20px;
-          }
-          .footer__social .separator {
-            display: none;
-          }
-          .container, .partenaire, .footer__links {
-            flex-direction:column;
-            align-items:center;
-            justify-content:center;
-            text-align:center;
-          }
-
-          .partenaire a {
-            display:flex;
-            flex-direction:column;
-            margin: 10px auto;
-          }
-        }
-      `}</style>
-    </>
   );
 };
 
