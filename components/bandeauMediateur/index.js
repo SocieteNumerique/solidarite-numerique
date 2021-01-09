2326cc046055ee18a3dd7429abbddd1cc27ab3b1
khalid62 <75318838+khalid62@users.noreply.github.com>
2021-01-09 20:39:38 +0100

    Update index.js

--- components/bandeauMediateur/index.js
+++ components/bandeauMediateur/index.js
@@ -7,7 +7,7 @@ import constants from '../../constants';
 const BandeauMediateur = () => (
   <>
     <div width = "100%" height = "410">  
-      <iframe src="https://cget-carto.github.io/France-services/" title="France services" width = "90%" height = "520"></iframe>
+      <iframe src="https://cget-carto.github.io/France-services/" title="France services" width = "90%" height = "520" margin:0 auto;></iframe>
     </div>
   
     <div className="bandeau-mediateur">
