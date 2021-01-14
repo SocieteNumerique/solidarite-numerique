ac3a90391c0dd7409d76eee1f36ce0aadc31659f
khalid62 <75318838+khalid62@users.noreply.github.com>
2021-01-14 21:39:10 +0100

    Update mentions-legales.js

--- pages/mentions-legales.js
+++ pages/mentions-legales.js
@@ -8,57 +8,24 @@ const MentionsLegales = () => {
       <div className="text-container">
         <h1>Mentions Légales</h1>
         <h2>Editeur</h2>
-        <p>
-          La MedNum - 135 BOULEVARD CHANZY - 93100 MONTREUIL
-          <br />
-          Adresse contact : contact@lamednum.coop
-          <br />
-          N° RCS : 831 673 892 R.C.S Bobigny
-          <br />
-          Capital social : 62,900 euros
-          <br />
-        </p>
+          <p>Le responsable de traitement est l’Agence Nationale de la Cohésion des Territoires, « ANCT », établissement public de l’Etat créé par la loi n° 2019-753 du 22 juillet 2019 et en application du décret n° 2019-1190 du 18 novembre 2019, immatriculée sous le numéro SIRENE 130 026 032 dont le siège est 20 avenue de Ségur – TSA 10717 – 75334 PARIS CEDEX 07, représenté par Monsieur Yves LE BRETON, Directeur Général de ladite Agence, nommé par décret du Président de la République en date du 23 décembre 2019 et domicilié en cette qualité audit siège,<br />
+          Ci-après dénomé « l’ANCT » <br />
+          Le délégué à la protection des données est Mme Anne Gaillard, ANCT 20 avenue de Ségur – TSA 10717 – 75334 PARIS CEDEX 07  <a href = "mailto: dpo@anct.gouv.fr">dpo@anct.gouv.fr</a>
+          </p>
         <h2>Directeur de la publication</h2>
-        <p>Emmanuel Vandamme, Président de la SCIC La MedNum</p>
-        <h2> Hébergeur </h2>
-        <p>
-          Zeit Now - 340 S Lemon Ave #4133, Walnut CA, 91789
-          <br />
-          Adresse contact : support@zeit.co
-          <br />
-        </p>
+          <p></p>
+        <h2>Hébergeur</h2>
+          <p></p>
         <h2>Illustrations</h2>
-        <p>
-          Visuel principal de la page d’accueil : un grand merci à Sarah
-          Lepreux.
-        </p>
-        <p>
-          Les informations que vous nous confiez sont enregistrées dans un fichier informatisé par L&apos;Agence Nationale de Cohésion des Territoires, 20, avenue de Ségur TSA 10717, 75 334 Paris Cedex 07<br />
-          La base légale du traitement de vos données est l’article 5.6 de la Loi n° 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés.<br />
-          Nous recueillons vos données dans la poursuite de notre intérêt légitime: l’amélioration du service que nous vous proposons pour répondre à vos besoins en accompagnement numérique.<br />
-          Ainsi, nous recueillons votre numéro de téléphone portable, afin de retrouver votre requête précédente dans le cas où vous nous rappeleriez, et de répondre au mieux à vos besoins. Ce recueil nous permet également de vous envoyer des ressources par SMS, si vous acceptez d’en recevoir.<br />
-          Nous collectons également, si vous acceptez de nous les confier:<br />
-
-            <li>Votre code postal, qui nous permettra d’identifier, par le biais d’une enquête statistique anonymisée, les besoins spécifiques en accompagnement numérique de chaque territoire;</li>
-            <li>Vos nom et prénom, afin de personnaliser le service que nous vous apportons;</li>
-            <li>Votre adresse mail, si vous souhaitez que nous vous envoyons des ressources.</li>
-
-          Nous conservons vos données pour une durée de trois ans suite à votre dernier appel.<br />
-          Vos données sont accessibles par:<br />
-    
-            <li>L&apos;Agence Nationale de Cohésion des territoires, responsable du traitement</li>
-            <li>Ses prestataires techniques, Aircall et la Régie de Quartiers de Trélazé, qui stockent et sécurisent vos données;</li>
-            <li>La MedNum, coordinateur du projet</li>
-            <li>Hubik et Hinaura, partenaires formation</li>
-            <li>Les médiateurs qui vous accompagnent.</li>
-    
-          Vous pouvez nous demander d’accéder à vos données personnelles, en demander la rectification ou l&apos;effacement, ou encore solliciter une limitation du traitement vous concernant.<br />
-          Vous pouvez également vous opposer au traitement et solliciter la portabilité de vos données.<br />
-          Pour exercer ces droits ou pour toute question sur le traitement de vos données dans ce dispositif, vous pouvez nous contacter à l’adresse suivante: dpo@anct.gouv.fr<br />
-          Si vous estimez, après nous avoir contactés, que vos droits &laquo; Informatique et Libertés &raquo; ne sont pas respectés, vous pouvez adresser une réclamation à la CNIL.<br />
-          Vous pouvez également consulter le site <a href="http://cnil.fr">cnil.fr</a> pour plus d’informations sur vos droits.<br />
-    
-        </p>
+          <p></p>
+        <h2>Message d’accueil</h2>
+          <p></p>
+        <h2>Message fermeture du service</h2>
+          <p></p>
+        <h2>[Fêtes] Message fermeture du service</h2>
+          <p></p>
+        <h2>Message d'indisponibilité</h2>
+          <p></p>
       </div>
     </Page>
   );
