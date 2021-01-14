9404930c4a1c70f826603a28109c9a766e2092a1
khalid62 <75318838+khalid62@users.noreply.github.com>
2021-01-14 21:49:15 +0100

    Update mentions-legales.js

--- pages/mentions-legales.js
+++ pages/mentions-legales.js
@@ -13,19 +13,27 @@ const MentionsLegales = () => {
           Le délégué à la protection des données est Mme Anne Gaillard, ANCT 20 avenue de Ségur – TSA 10717 – 75334 PARIS CEDEX 07  <a href = "mailto: dpo@anct.gouv.fr">dpo@anct.gouv.fr</a>
           </p>
         <h2>Directeur de la publication</h2>
-          <p></p>
+          <p>Pierre-Louis ROLLE, Directeur Programmes « Société Numérique » et « Nouveaux Lieux Nouveaux Liens » à l’Agence Nationale de Cohésion des Territoires « ANCT »
+          </p>
         <h2>Hébergeur</h2>
-          <p></p>
+          <p>Zeit Now - 340 S Lemon Ave #4133, Walnut CA, 91789<br />
+          Adresse contact : <a href = "mailto: support@zeit.co">support@zeit.co</a>
+          </p>
         <h2>Illustrations</h2>
-          <p></p>
+          <p>Visuel principal de la page d’accueil : un grand merci à Sarah Lepreux.</p>
+        <h1>Serveur vocal</h1>
         <h2>Message d’accueil</h2>
           <p></p>
         <h2>Message fermeture du service</h2>
-          <p></p>
+          <p>Bonjour et merci de votre appel. Solidarité Numérique est fermé pour le moment. Nous pouvons vous accompagner pour vos démarches en ligne du lundi au vendredi de 9h à 18h.<br />
+          (Vous trouverez de l&apos;aide sur notre site internet solidarité-numerique.fr)
+          </p>
         <h2>[Fêtes] Message fermeture du service</h2>
-          <p></p>
-        <h2>Message d'indisponibilité</h2>
-          <p></p>
+          <p>Bonjour et merci de votre appel. Solidarité Numérique est fermé pour le moment.  Durant cette période de fin d’année nous pouvons vous accompagner pour vos démarches en ligne du lundi au jeudi de 11h à 16h.<br />
+          A très vite !
+          </p>
+        <h2>Message d&apos;indisponibilité</h2>
+          <p>Tous nos médiateurs numériques sont actuellement en communication. Merci de nous rappeler ultérieurement. A très vite !</p>
       </div>
     </Page>
   );
