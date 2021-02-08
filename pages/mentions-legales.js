35dc8d9f0ec5665ba44c3aaa11d6d9ec594aef1d
khalid62 <75318838+khalid62@users.noreply.github.com>
2021-02-08 10:06:33 +0100

    Update mentions-legales.js

--- pages/mentions-legales.js
+++ pages/mentions-legales.js
@@ -12,7 +12,7 @@ const MentionsLegales = () => {
             Ci-après dénomé « l’ANCT » <br>
             Le délégué à la protection des données, ANCT 20 avenue de Ségur – TSA 10717 – 75334 PARIS CEDEX 07 <a href="mailto:dpo@anct.gouv.fr">dpo@anct.gouv.fr</a></p>
         <h2>Directeur de la publication</h2>
-          <p>Pierre-Louis ROLLE, Directeur Programmes « Société Numérique » et « Nouveaux Lieux Nouveaux Liens » à l’Agence Nationale de Cohésion des Territoires « ANCT »
+          <p>Pierre-Louis ROLLE, Directeur Programmes « Société Numérique » et « Nouveaux Lieux Nouveaux Liens » à l’Agence Nationale de Cohésion des Territoires « ANCT » 
           </p>
         <h2>Hébergeur</h2>
           <p>Zeit Now - 340 S Lemon Ave #4133, Walnut CA, 91789<br />
