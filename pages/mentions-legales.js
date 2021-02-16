8dc1ec67b2178261fd84c26fb16452a561d4f77c
khalid62 <75318838+khalid62@users.noreply.github.com>
2021-02-16 13:27:13 +0100

    Update mentions-legales.js

--- pages/mentions-legales.js
+++ pages/mentions-legales.js
@@ -8,9 +8,10 @@ const MentionsLegales = () => {
       <div className="text-container">
         <h1>Mentions Légales</h1>
         <h2>Editeur</h2>
-          <p><br>
+          <p>Le responsable de traitement est l’Agence Nationale de la Cohésion des Territoires, « ANCT », établissement public de l’Etat créé par la loi n° 2019-753 du 22 juillet 2019 et en application du décret n° 2019-1190 du 18 novembre 2019, immatriculée sous le numéro SIRENE 130 026 032 dont le siège est 20 avenue de Ségur – TSA 10717 – 75334 PARIS CEDEX 07, représenté par Monsieur Yves LE BRETON, Directeur Général de ladite Agence, nommé par décret du Président de la République en date du 23 décembre 2019 et domicilié en cette qualité audit siège,<br>
             Ci-après dénomé « l’ANCT » <br>
-            Le délégué à la protection des données, ANCT 20 avenue de Ségur – TSA 10717 – 75334 PARIS CEDEX 07 <a href="mailto:dpo@anct.gouv.fr">dpo@anct.gouv.fr</a></p>
+            Le délégué à la protection des données, ANCT 20 avenue de Ségur – TSA 10717 – 75334 PARIS CEDEX 07 <a href = "mailto:dpo@anct.gouv.fr">dpo@anct.gouv.fr</a>
+          </p>
         <h2>Directeur de la publication</h2> 
           <p>Pierre-Louis ROLLE, Directeur Programmes « Société Numérique » et « Nouveaux Lieux Nouveaux Liens » à l’Agence Nationale de Cohésion des Territoires « ANCT »</p>
         <h2>Hébergeur</h2>
