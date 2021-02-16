12559dfa04de43f66d3a182f7b4919563a895220
khalid62 <75318838+khalid62@users.noreply.github.com>
2021-02-16 13:38:53 +0100

    Update index.js

--- components/bandeauMediateur/index.js
+++ components/bandeauMediateur/index.js
@@ -20,7 +20,7 @@ const BandeauMediateur = () => (
               target="_blank"
             >
               <img className="fs1-align"
-                src="/images/logo/CN.png"
+                src="/images/logo/LCN.png"
                 alt="Conseiller Numérique "
               />
               <p>abc Vous aimez le contact humain, cherchez un métier qui a du sens dans un secteur d’avenir ? Rejoignez le mouvement national des conseillers numériques !</p>
