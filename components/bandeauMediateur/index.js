b777f9909d9cd99730692fc0eabc20432223841b
khalid62 <75318838+khalid62@users.noreply.github.com>
2021-02-16 12:48:26 +0100

    Update index.js
    changed path of the image

--- components/bandeauMediateur/index.js
+++ components/bandeauMediateur/index.js
@@ -20,7 +20,7 @@ const BandeauMediateur = () => (
               target="_blank"
             >
               <img className="fs1-align"
-                src="/images/logo/CN.png"
+                src="/images/logo/logo-conseiller-numerique.png 2021.png.png"
                 alt="Conseiller Numérique "
               />
               <p>Vous aimez le contact humain, cherchez un métier qui a du sens dans un secteur d’avenir ? Rejoignez le mouvement national des conseillers numériques !</p>
